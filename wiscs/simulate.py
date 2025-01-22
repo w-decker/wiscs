7bd3e10a6b449a0aecb0fad68958a8a8f5668f39
Will Decker <will.decker@gatech.edu>
2025-01-22 15:42:30 -0500

    update with correct implementation of random effects simulatoins

--- wiscs/simulate.py
+++ wiscs/simulate.py
@@ -6,81 +6,89 @@ import warnings
 import numpy.typing as npt
 import pandas as pd
 import copy
+from typing import Union
 
 import numpy as np
 import warnings
 import numpy.typing as npt
+
+MODALITIES = ["word", "image"]
+
+def _random_effects(sigma:npt.ArrayLike, size:Union[int, tuple], mean:npt.ArrayLike=[0, 0]) -> npt.ArrayLike:
+    """Generate random effects"""
+    v = np.random.multivariate_normal(mean, sigma, size)
+    intercept = v[:, 0]
+    slope = v[:, 1]
+    return intercept, slope
 
 def generate(params:dict, seed:int=None):
 
     if seed is not None:
         np.random.seed(seed)
 
-    # get experimental parameters
-    n_sub = params["n"]["subject"]
-    n_ques = params["n"]["question"]
-    n_item = params["n"]["trial"]
+    n_subs = params["n"]["subject"]
+    n_questions = params["n"]["question"]
+    n_items = params["n"]["trial"]
 
     w_perceptual = params["word"]["perceptual"]
     w_conceptual = params["word"]["conceptual"]
-    w_task_times = params["word"]["task"]
+    w_task = params["word"]["task"]
 
     i_perceptual = params["image"]["perceptual"]
     i_conceptual = params["image"]["conceptual"]
-    i_task_times = params["image"]["task"]
+    i_task = params["image"]["task"]
 
-    Sigma_sub   = params["var"]["subject"]   # shape (2,2)
-    Sigma_ques  = params["var"]["question"]  # shape (2,2)
-    Sigma_item  = params["var"]["item"]      # shape (2,2)
-    sigma_error = params["var"]["error"]     # float
+    # variance components
+    sd_question = params["var"]["question"]
+    sd_item = params["var"]["trial"]
+    cov_subject = params["var"]["subject"] # 2 x 2 covariance matrix for subject
+    error = params["var"]["error"]
 
-    uv_sub = np.random.multivariate_normal(mean=[0, 0], cov=Sigma_sub, size=n_sub)
-    sub_u = uv_sub[:, 0]  # random intercept for subject
-    sub_v = uv_sub[:, 1]  # random slope
+    # Seed for reproducibility
+    np.random.seed(123)
 
-    uv_ques = np.random.multivariate_normal(mean=[0, 0], cov=Sigma_ques, size=n_ques)
-    ques_u = uv_ques[:, 0]  # random intercept for question
-    ques_v = uv_ques[:, 1]  # random slope
+    # generate variance structures
+    beta0, beta1 = _random_effects(cov_subject, n_subs) # random slope and intercept for subjects
+    question_effects = np.random.normal(0, sd_question, n_questions)
+    item_effects = np.random.normal(0, sd_item, n_items)
 
-    uv_item = np.random.multivariate_normal(mean=[0, 0], cov=Sigma_item, size=n_item)
-    item_u = uv_item[:, 0]  # random intercept for item
-    item_v = uv_item[:, 1]  # random slope
+    # fixed RT without noise
+    fixed_rt_word = w_perceptual + w_conceptual + w_task
+    fixed_rt_image = i_perceptual + i_conceptual + i_task
 
-    S_grid, Q_grid, I_grid = np.meshgrid(
-        np.arange(n_sub),
-        np.arange(n_ques),
-        np.arange(n_item),
-        indexing='ij'
+    # reshape
+    beta0 = beta0[:, np.newaxis, np.newaxis]
+    beta1 = beta1[:, np.newaxis, np.newaxis]
+    fixed_rt_word = fixed_rt_word[np.newaxis, :, np.newaxis]
+    fixed_rt_image = fixed_rt_image[np.newaxis, :, np.newaxis]
+    question_effects = question_effects[np.newaxis, :, np.newaxis]
+    item_effects = item_effects[np.newaxis, np.newaxis, :]
+    q_indices = np.arange(1, n_questions + 1).reshape(1, n_questions, 1)
+
+    # slope
+    slope_q = beta1 * q_indices
+
+    # residual/error
+    residual_word = np.random.normal(0, error, size=(n_subs, n_questions, n_items))
+    residual_image = np.random.normal(0, error, size=(n_subs, n_questions, n_items))
+
+    word = (
+        fixed_rt_word          # Fixed components for WORD
+        + beta0                # Subject random intercept
+        + slope_q              # Subject random slope effect
+        + question_effects     # Question random effects
+        + item_effects         # Item random effects
+        + residual_word        # Residual noise
     )
 
-    word_random = (
-        sub_u[S_grid]          # subject intercept
-        + ques_u[Q_grid]       # question intercept
-        + item_u[I_grid]       # item intercept
+    image = (
+        fixed_rt_image         # Fixed components for IMAGE
+        + beta0                # Subject random intercept
+        + slope_q              # Subject random slope effect
+        + question_effects     # Question random effects
+        + item_effects         # Item random effects
+        + residual_image       # Residual noise
     )
-    # plus a distinct residual error for each cell
-    word_error = np.random.normal(0, sigma_error, size=(n_sub, n_ques, n_item))
-
-    image_random = (
-        (sub_u[S_grid] + sub_v[S_grid])       # subject intercept + slope
-        + (ques_u[Q_grid] + ques_v[Q_grid])   # question intercept + slope
-        + (item_u[I_grid] + item_v[I_grid])   # item intercept + slope
-    )
-    image_error = np.random.normal(0, sigma_error, size=(n_sub, n_ques, n_item))
-
-    word_base = (
-        w_perceptual
-        + w_conceptual
-        + w_task_times[Q_grid]  # shape (n_sub, n_ques, n_item)
-    )
-    image_base = (
-        i_perceptual
-        + i_conceptual
-        + i_task_times[Q_grid]
-    )
-
-    word = word_base + word_random + word_error
-    image = image_base + image_random + image_error
 
     return word, image
 
@@ -104,7 +112,7 @@ class DataGenerator(object):
     def __init__(self):
         self.params = copy.deepcopy(config.p)
 
-    def fit_transform(self, params:dict=None, overwrite:bool=False):
+    def fit_transform(self, params:dict=None, overwrite:bool=False, seed:int=None):
         if np.array_equal(self.params["word"]["task"], self.params["image"]["task"]):
             warnings.warn("Simulating data for MAIN hypothesis.")
         else:
@@ -115,20 +123,20 @@ class DataGenerator(object):
                 raise ValueError("If overwrite is True, params must be provided")
             elif params is not None and len(params) != len(self.params):
                 self.params = update_params(self.params, params)
-                self.data = generate(self.params)
+                self.data = generate(self.params, seed=seed)
             elif params is not None and len(params) == len(self.params):
                 validate_params(params)
                 self.params = parse_params(params)
-                self.data = generate(self.params)
+                self.data = generate(self.params, seed=seed)
         else:
             if params is not None and len(params) == len(self.params):
                 validate_params(params)
-                self.data = generate(parse_params(params))
+                self.data = generate(parse_params(params), seed=seed)
             elif params is not None and len(params) != len(self.params):
                 params = update_params(self.params, params)
-                self.data = generate(params)
+                self.data = generate(params, seed=seed)
             else:
-                self.data = generate(self.params)
+                self.data = generate(self.params, seed=seed)
 
         return self    
     
