75df96ac5c6a2a99ab76811e0f04e433e9ab75ba
Will Decker <will.decker@gatech.edu>
2025-02-27 15:37:07 -0500

    `v3.0.1`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wiscs",
-    version="3.0.0",
+    version="3.0.1",
     author="Will Decker",
     author_email="will.decker@gatech.edu",
     description="Simulating data for the project, Words and Images in Shared Concpetual Space",
